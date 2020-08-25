3336676d2e103cf9ea0258d27cd9f148669b45be
xiong <727257758@qq.com>
2020-08-25 21:54:52 +0800

    删除主文件多余代码

--- a53/main.c
+++ a53/main.c
@@ -6,66 +6,6 @@
 #include"sqlite.h"
 #include"common.h"
 
-/*
-typedef struct
-{
-	char data[64];
-	int argc;
-}sLog;
-
-void *SelectLog(void *arg)
-{
-	sDes *des = (sDes *)arg;
-	key_t key;
-	int shmid;
-	int semid;
-	sLog *psLog;
-	
-	key = ftok("/project2",0x120);
-	
-	if(key == -1)
-	{
-		perror("key");
-	}else
-	{
-		printf("%s ftok ok key:%d\n",__FUNCTION__,key);
-	}
-
-	shmid = shmget(key,sizeof(psLog)*100+4,IPC_CREAT|0666);
-	if(shmid == -1)
-	{
-		perror("shmget");
-	}else
-	{
-		printf("%s shmget ok key:%d\n",__FUNCTION__,shmid);
-	}
-
-	semid = semget(key, 1,IPC_CREAT|0666);
-	if(semid == -1)
-	{
-		perror("semget");
-	}else
-	{
-		printf("%s semget ok key:%d\n",__FUNCTION__,semid);
-	}
-
-	init_sem(semid, 1);
-
-	psLog = (sLog *)shmat(shmidm,NULL,0);
-
-	while(1)
-	{
-		sem_p(semid);
-			while(!strncmp(psLog,"OK",2));
-			Select(des.db,"cmd","a");
-
-
-		sem_v(semid);
-	
-	}
-
-}
-*/
 
 int main(int argc, const char *argv[])
 {
